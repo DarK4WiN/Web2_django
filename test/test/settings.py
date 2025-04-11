4ab69cd336242d47d476b1b515dcca9c44f9699a
DarK4WiN <nebizade2006@gmail.com>
2025-04-11 11:04:10 +0400

    allowed host deyisdi

--- test/test/settings.py
+++ test/test/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cr0lhmnj5+p85*=&m+6sza%nmz(f_wn$=3pjd8+x&m7n(qzg^@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 
 # Application definition
